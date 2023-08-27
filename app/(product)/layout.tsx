935a7a5c32df64398eca71ebcc3f5039a5c2216e
navi-05 <naveenthatsmyname5@gmail.com>
2023-08-27 11:29:37 +0530

    Changed synchronous script

--- app/(product)/layout.tsx
+++ app/(product)/layout.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script"
+
 import ModalProvider from "@/providers/ModalProvider"
 
 const ProductLayout = ({ children }: { children: React.ReactNode}) => {
@@ -5,7 +7,7 @@ const ProductLayout = ({ children }: { children: React.ReactNode}) => {
       <div>
         <ModalProvider />
         {children}
-        <script src={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`}></script>
+        <Script src={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`} async />
       </div>
   )
 }
